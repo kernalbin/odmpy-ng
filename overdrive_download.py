db386b356b13a80122d00c9d34a4cea78a80c49f
kernalbin <sphunts01@gmail.com>
2025-05-21 10:03:19 -0600

    clean up downloader

--- overdrive_download.py
+++ overdrive_download.py
@@ -1,29 +1,8 @@
-import requests, collections, os, json
+import requests, os, json
 import convert_metadata
 
 headers = {'User-Agent': 'Mozilla/5.0'}
 
-
-def downloadMP3(book_urls, download_path, cookies):
-    ordered_urls = collections.OrderedDict(sorted(book_urls.items()))
-    if not os.path.exists(download_path):
-        os.makedirs(download_path)
-
-    cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
-
-    for part_id, audio_file in ordered_urls.items():
-        print("Downloading part " + str(int(part_id)))
-        response = requests.get(audio_file, headers=headers, cookies=cookie_dict, stream=True)
-
-        if response.status_code == 200:
-            with open(os.path.join(download_path, f"part{part_id}.mp3"), "wb") as f:
-                for chunk in response.iter_content(1024):
-                    f.write(chunk)
-        else:
-            print(f"Download failed: {response.status_code}")
-            return False
-        
-    return True
 
 def downloadMP3Part(url, part_id, download_path, cookies) -> int: # Downloads mp3 part to download_path/part[ID].mp3 and returns the length in seconds
     if not os.path.exists(download_path):
@@ -40,11 +19,11 @@ def downloadMP3Part(url, part_id, download_path, cookies) -> int: # Downloads mp
                 f.write(chunk)
         return convert_metadata.get_mp3_duration(os.path.join(download_path, f"part{part_id}.mp3"))
     else:
-        print(f"Download failed: {response.status_code}")
+        print(f"Failed to download mp3 part with status code {response.status_code}")
         return 0
 
 
-def downloadCover(cover_url, download_path, cookies):
+def downloadCover(cover_url, download_path, cookies): # Downloads cover image to download_path and returns True if successful
 
     cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
 
@@ -54,9 +33,10 @@ def downloadCover(cover_url, download_path, cookies):
             for chunk in response.iter_content(1024):
                 f.write(chunk)
         return True
+    print(f"Failed to download cover with status code {response.status_code}")
     return False
 
-def downloadThunderMetadata(book_id: int, download_path):
+def downloadThunderMetadata(book_id: int, download_path): # Downloads thunder API book metadata to download_path
     api_url = f"https://thunder.api.overdrive.com/v2/media/{book_id}"
 
     response = requests.get(api_url)
